83d68cd10be4914448764f3a10e6d81fa14aff81
Proxyy587 <abhijitbhattacharjee333@gmail.com>
2024-06-19 00:09:17 +0530

    project updated

--- constant/index.ts
+++ constant/index.ts
@@ -1,61 +1,62 @@
 export const navItems = [
-    {
-        title: "~",
-        url: "/",
-    },
-    {
-        title: "about",
-        url: "/about",
-    },
-    {
-        title: "writing",
-        url: "/writing",
-    },
-]
+  {
+    title: "~",
+    url: "/",
+  },
+  {
+    title: "about",
+    url: "/about",
+  },
+  {
+    title: "writing",
+    url: "/writing",
+  },
+];
 
 export const socialLinks = [
-    {
-        name: 'twitter',
-        url: 'https://x.com/proxyxd_s',
-    },
-    {
-        name: 'github',
-        url: 'https://github.com/Proxyy587',
-    },
-    {
-        name: 'linkedin',
-        url: 'https://www.linkedin.com/in/invaliduser/',
-    },
-    {
-        name: 'instagram',
-        url: 'https://www.instagram.com/ptrdoxy/',
-    },
-]
+  {
+    name: "twitter",
+    url: "https://x.com/proxyxd_s",
+  },
+  {
+    name: "github",
+    url: "https://github.com/Proxyy587",
+  },
+  {
+    name: "linkedin",
+    url: "https://www.linkedin.com/in/invaliduser/",
+  },
+  {
+    name: "instagram",
+    url: "https://www.instagram.com/ptrdoxy/",
+  },
+];
 
 export const heroProjectsContent = [
-    {
-        title: 'proxiee',
-        description: 'its my portfolio. this website you are in right now.',
-        url: 'https://github.com/Proxyy587/proxie',
-    },
-    {
-        title: 'email-newsletter',
-        description: 'A newsletter email service app.',
-        url: 'https://github.com/Proxyy587/email-newsletter',
-    },
-    {
-        title: 'blog site',
-        description: 'A good looking awwwards blog site.',
-        url: 'https://github.com/Proxyy587/vugo',
-    },
-    {
-        title: 'cypro studio',
-        description: 'its my startup agency for development.',
-        url: 'https://github.com/Proxyy587/Cypro',
-    },
-    {
-        title: 'ecommerce solution',
-        description: 'an fullstack ecommerce project with payload cms.',
-        url: 'https://github.com/Proxyy587/e-commerce',
-    },
+  {
+    title: "proxiee",
+    description: "its my portfolio. this website you are in right now.",
+    url: "https://github.com/Proxyy587/",
+  },
+  {
+    title: "cypro studio",
+    description: "its my startup agency for development.",
+    url: "https://www.cyprostudio.com",
+  },
+  {
+    title: "email-newsletter",
+    description: "A newsletter email service app.",
+    url: "https://sparkpost.vercel.app/",
+  },
+  {
+    title: "svrjs website rebrand",
+    description:
+      "SVR.JS is a web server that runs on top of Node.JS, thus enabling server-side JavaScript on webpages.",
+    url: "https://svrjs.vercel.app/",
+  },
+  {
+    title: "blog site",
+    description: "A good looking awwwards blog site.",
+    url: "https://vugo-8kka.vercel.app/",
+  },
 ];
